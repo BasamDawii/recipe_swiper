110c497e4aa78c028fc2f5537f7c1b811b911430
BasamDawii <basa0033@easv365.dk>
2024-05-24 11:58:16 +0200

    first committ

---
+++ android/app/src/main/kotlin/com/example/recipe_swiper/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.recipe_swiper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/views/widgets/would_you_rather_widget.dart
@@ -0,0 +1,60 @@
+// would_you_rather_widget.dart
+import 'package:flutter/material.dart';
+import 'package:recipe_swiper/models/recipe.dart';
+import 'package:recipe_swiper/views/widgets/recipe_card.dart';
+
+class WouldYouRatherWidget extends StatelessWidget {
+  final Recipe optionA;
+  final Recipe optionB;
+  final Function(Recipe) onOptionSelected;
+
+  const WouldYouRatherWidget({
+    Key? key,
+    required this.optionA,
+    required this.optionB,
+    required this.onOptionSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: GestureDetector(
+            onTap: () => onOptionSelected(optionA),
+            child: RecipeCard(
+              title: optionA.name,
+              cookTime: optionA.totalTime,
+              rating: optionA.rating.toString(),
+              thumbnailUrl: optionA.images,
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.black,
+          child: Center(
+            child: Text(
+              'OR',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () => onOptionSelected(optionB),
+            child: RecipeCard(
+              title: optionB.name,
+              cookTime: optionB.totalTime,
+              rating: optionB.rating.toString(),
+              thumbnailUrl: optionB.images,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
